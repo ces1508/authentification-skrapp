f70749124506374217d90454c437ebfdd8fed98e
christian segura <ces1508@gmail.com>
2018-03-13 11:26:04 -0500

    verificar que el cliente tenga [ermiso para utilizar la api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jwtOptions = {}
 const SECRET = process.env.SECRET_TOKEN
 jwtOptions.jwtFromRequest = ExtactJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = SECRET
+const APIKEY = process.env
 const request = require('request')
 
 const Strategy = new JwtStrategy(jwtOptions, (payload, done) => {
@@ -19,5 +20,11 @@ const Strategy = new JwtStrategy(jwtOptions, (payload, done) => {
     return done(null, user)
   })
 })
+
+exports.verifyClient = (req, res, next) => {
+  let { headers } = req
+  if (!headers['x-api-key'] || headers['x-api-key'] !== APIKEY) res.status(401).json({ error: true, message: 'Unauthorized' })
+  next()
+}
 
 module.exports = Strategy
